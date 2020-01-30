bd596d66a64799e98858e5e45fa893b1dce7807f
Agnieszka Olech <agnieszka.anstazja.olech@gmail.com>
2020-01-30 16:54:26 +0100

    Define urls, create view, add template

--- public_holidays/holiday_core/views.py
+++ public_holidays/holiday_core/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from .models import PublicHoliday
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import PublicHoliday, Country
+from django.views.generic.list import ListView
+from django.conf import settings
 
 
 def index(request):
-    holidays = PublicHoliday.objects.all()
-    context = {'holidays': holidays}
-    return render(request, 'holiday_core/index.html', context)
+    default_country = Country.objects.get(code=settings.DEFAULT_COUNTRY)
+    default_year = settings.DEFAULT_YEAR
+    return redirect('main_view', country_id=default_country.id, year=default_year)
+
+
+class HolidaysCountryYear(ListView):
+
+    model = PublicHoliday
+    template_name = 'holiday_core/index.html'
+    context_object_name = 'holidays'
+
+    def get_queryset(self):
+        country_id = self.kwargs['country_id']
+        year = self.kwargs['year']
+        qs = PublicHoliday.objects.filter(
+            country_id=country_id,
+            holiday_date__year=year
+        )
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['country_id'] = self.kwargs['country_id']
+        context['year'] = self.kwargs['year']
+        return context
--- public_holidays/public_holidays/urls.py
+++ public_holidays/public_holidays/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from holiday_core import views
-from django.core.management import call_command
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<int:country_id>/<int:year>/', views.HolidaysCountryYear.as_view(), name='main_view'),
 ]
 
-call_command('get_holiday_data')
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
